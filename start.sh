d579329fcca056c4620e23930e1926847994e626
ja5aea <take.tsutsumi@gmail.com>
2019-02-26 15:11:10 +0900

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+apps_dir=/media/mmcblk0p1/apps
+
+source $apps_dir/stop.sh
+
+cat $apps_dir/sdr_transceiver_hpsdr/sdr_transceiver_hpsdr.bit > /dev/xdevcfg
+
+<< JA5AEA
+6th argument
+1: CAT Kenwood 4,800bps
+2: CAT Kenood 38,400bps
+7th argument
+1: audio codec board clock 12.288MHz
+2: audio codec board clock 12.000MHz
+JA5AEA
+
+$apps_dir/sdr_transceiver_hpsdr/sdr-transceiver-hpsdr 1 2 2 1 2 1 2 &
+
